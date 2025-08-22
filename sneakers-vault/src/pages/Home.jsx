8cd7f619c1e08cf60588db78042a846942c7131a
alelodato <alss.ldt@gmail.com>
2025-08-22 14:51:05 +0200

    added hero image to homepage

--- sneakers-vault/src/pages/Home.jsx
+++ sneakers-vault/src/pages/Home.jsx
@@ -1,17 +1,24 @@
 import styles from "./Home.module.css";
 
 export default function Home() {
-    return (
+  return (
     <>
-    <div className={styles.homeintro}>
-        <h2 className={styles.hometitle}>Open the vault.
-            <br />Step into style.</h2>
-    </div>
-    <div className={styles.homebuttons}>
-        <button>MEN</button>
-        <button>WOMEN</button>
-        <button>KIDS</button>
-    </div>
+      <div className={styles.homeintro}>
+        <div className={styles.homecover}>
+          <div>
+            <h2 className={styles.hometitle}>
+              Open the vault.
+              <br />
+              Step into style.
+            </h2>
+          </div>
+          <div className={styles.homebuttons}>
+            <button>MEN</button>
+            <button>WOMEN</button>
+            <button>KIDS</button>
+          </div>
+        </div>
+      </div>
     </>
-    );
+  );
 }
