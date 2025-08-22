2499a65824f632f3336d2c49b7b0d04ea36bfe85
alelodato <alss.ldt@gmail.com>
2025-08-22 13:32:11 +0200

    added buttons to homepage to redirect to shop page based on filter

--- sneakers-vault/src/pages/Home.jsx
+++ sneakers-vault/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import styles from "./Home.module.css";
 
 export default function Home() {
-    return <div className={styles.home}>
+    return (
+    <>
+    <div className={styles.homeintro}>
         <h2 className={styles.hometitle}>Open the vault.
             <br />Step into style.</h2>
-    </div>;
+    </div>
+    <div className={styles.homebuttons}>
+        <button>Men</button>
+        <button>Women</button>
+        <button>Kids</button>
+    </div>
+    </>
+    );
 }
