9a075f99c4038755834ef1731c5f1ee16a32f710
alelodato <alss.ldt@gmail.com>
2025-08-22 21:57:02 +0200

    added sale and new arrivals and additional homepage elements at the bottom

--- sneakers-vault/src/pages/Home.jsx
+++ sneakers-vault/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import styles from "./Home.module.css";
 import ProductRotator from "../components/ProductRotator";
 import { products } from "../data/products";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -9,7 +10,7 @@ export default function Home() {
         <div className={styles.homecover}>
           <div>
             <h2 className={styles.hometitle}>
-              Open the vault.
+              Unlock the vault.
               <br />
               Step into style.
             </h2>
@@ -20,7 +21,18 @@ export default function Home() {
             <button>KIDS</button>
           </div>
           {/* Rotatore con 3 prodotti */}
+          <h3 className={styles.hot}>🔥HOTTEST ON THE MARKET🔥</h3>
           <ProductRotator products={products} />
+          <p className={styles.more}>SEE MORE <i class="fa-regular fa-circle-right"></i></p>
+          <h3 className={styles.sale}>ITEMS ON SALE (UP TO 70%)</h3>
+          <ProductRotator products={products} />
+          <p className={styles.more}>SEE MORE <i class="fa-regular fa-circle-right"></i></p>
+          <h3 className={styles.sale}>NEW ARRIVALS</h3>
+          <ProductRotator products={products} />
+          <p className={styles.more}>SEE MORE <i class="fa-regular fa-circle-right"></i></p>
+          <h4 className={styles.newsletter}>Sign Up to our newsletter <i class="fa-solid fa-angle-down"></i></h4>
+          <h4 className={styles.contact}><Link className={styles.link} to={'Contact'}>Contact Us</Link></h4>
+          <h4 className={styles.lang}>English <i class="fa-solid fa-angle-down"></i></h4>
         </div>
       </div>
     </>
