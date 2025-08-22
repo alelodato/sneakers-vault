859746c63af9d32ae67eaf6186378a2ff83a38d9
alelodato <alss.ldt@gmail.com>
2025-08-22 11:55:51 +0200

    created navbar component

---
+++ sneakers-vault/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import { FaBars, FaSearch, FaHeart, FaShoppingBag } from "react-icons/fa";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+    return (
+        <header className={styles.header}>
+            {/* Left: Menu + Search */}
+            <div className={styles.left}>
+                <FaBars size={22} className={styles.icon} />
+                <FaSearch size={22} className={styles.icon} />
+            </div>
+
+            {/* Center: Logo */}
+            <h1 className={styles.logo}>Sneakers Vault</h1>
+
+            {/* Right: Favourites + Cart */}
+            <div className={styles.right}>
+                <FaHeart size={22} className={styles.icon} />
+                <FaShoppingBag size={22} className={styles.icon} />
+            </div>
+            </header>
+    );
+}
