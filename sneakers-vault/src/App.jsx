c0a94e530d9795fd2dfbe0545763dc548763c320
alelodato <alss.ldt@gmail.com>
2025-08-22 12:11:44 +0200

    adjusted index.css styles and added styles to navbar

--- sneakers-vault/src/App.jsx
+++ sneakers-vault/src/App.jsx
@@ -7,13 +7,13 @@ import './App.css'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
     <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/checkout" element={<CheckOut />} />
     </Routes>
-    </BrowserRouter>
+    </>
   );
 }
---
+++ sneakers-vault/src/pages/CheckOut.jsx
@@ -0,0 +1,3 @@
+export default function CheckOut() {
+    return <h2>Checkout</h2>;
+}
