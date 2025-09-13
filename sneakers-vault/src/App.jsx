7f014fc5a32581b6943c39fb45ecb12817127455
alelodato <alss.ldt@gmail.com>
2025-09-13 12:10:42 +0200

    created contact page and added translations

--- sneakers-vault/src/App.jsx
+++ sneakers-vault/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact.jsx";
 import Shop from "./pages/Shop";
 import Checkout from "./pages/Checkout.jsx";
 import OnSale from "./pages/OnSale";
@@ -10,8 +11,9 @@ import Cart from "./pages/Cart";
 import Wishlist from "./pages/WishList";
 import Success from "./pages/Success";
 import Cancel from "./pages/Cancel";
-import "./App.css";
 import ProductPage from "./pages/ProductPage";
+import "./App.css";
+
 
 export default function App() {
   return (
@@ -20,6 +22,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/wish" element={<Wishlist />} />
         <Route path="/trend" element={<Trending />} />
---
+++ sneakers-vault/src/pages/Contact.jsx
@@ -0,0 +1,61 @@
+import styles from "./Contact.module.css";
+import { useTranslation } from "react-i18next";
+import { FaInstagram, FaTwitter, FaThreads } from "react-icons/fa6";
+
+export default function Contact() {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.contactPage}>
+      <div className={styles.coverText}>
+        <h2 className={styles.title}>{t("contact.title")}</h2>
+
+        <form className={styles.contactForm}>
+          <label>
+            {t("contact.name")}
+            <input
+              type="text"
+              placeholder={t("contact.namePlaceholder")}
+              required
+            />
+          </label>
+
+          <label>
+            {t("contact.email")}
+            <input
+              type="email"
+              placeholder={t("contact.emailPlaceholder")}
+              required
+            />
+          </label>
+
+          <label>
+            {t("contact.message")}
+            <textarea placeholder={t("contact.messagePlaceholder")} required />
+          </label>
+
+          <button type="submit">{t("contact.submit")}</button>
+        </form>
+
+        <div className={styles.details}>
+          <h3>{t("contact.others")}</h3>
+          <p>{t("contact.phone")}: +44 7435 206 405</p>
+          <p>{t("contact.mail")}: info@sneakersvault.com</p>
+
+          <h3>{t("contact.follow")}</h3>
+          <div className={styles.socialIcons}>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <FaTwitter />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <FaThreads />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
