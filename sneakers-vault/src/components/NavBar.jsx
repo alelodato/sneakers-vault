8db09300bb55e5ce0dcadf0046b8a93377b9139c
alelodato <alss.ldt@gmail.com>
2025-09-22 14:15:35 +0700

    added aria-label to navbar icons

--- sneakers-vault/src/components/NavBar.jsx
+++ sneakers-vault/src/components/NavBar.jsx
@@ -1,9 +1,8 @@
 import styles from "./NavBar.module.css";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function NavBar() {
     const navigate = useNavigate();
-    const { pathname } = useLocation();
 
     const openSearch = () => {
         navigate({ pathname: "/", search: "?search=1" });
@@ -13,9 +12,9 @@ export default function NavBar() {
             {/* Left: Menu + Search */}
             <div className={styles.left}>
                 <Link className={styles.link} to="/shop">
-                <i class="fa-solid fa-shop"></i>
+                <i class="fa-solid fa-shop" aria-label="shop-icon"></i>
                 </Link>
-                <i class="fa-solid fa-magnifying-glass"
+                <i class="fa-solid fa-magnifying-glass" aria-label="searcbar-icon"
                 onClick={openSearch}>
 
                 </i>
@@ -28,10 +27,10 @@ export default function NavBar() {
             {/* Right: Favourites + Cart */}
             <div className={styles.right}>
                 <Link className={styles.link} to="/wish">
-                <i class="fa-regular fa-heart"></i>
+                <i class="fa-regular fa-heart" aria-label="wishlist-icon"></i>
                 </Link>
                 <Link className={styles.link} to="/cart">
-                <i class="fa-solid fa-cart-shopping"></i>
+                <i class="fa-solid fa-cart-shopping" aria-label="cart-icon"></i>
                 </Link>
             </div>
             </header>
